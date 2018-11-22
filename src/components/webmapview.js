acaf439c7d6db9cfaf4004604d6ead5360d13b42
giscrazier <774841712@qq.com>
2018-11-22 10:44:39 +0800

    test projection

--- src/components/webmapview.js
+++ src/components/webmapview.js
@@ -1,6 +1,26 @@
 import SceneView from "esri/views/SceneView";
 import React from "react";
 
+import Point from 'esri/geometry/Point';
+import SpatialReference from "esri/geometry/SpatialReference";
+
+const projection = require("esri/geometry/projection");
+const projectionPromise = projection.load();
+
+projectionPromise.then(()=>{
+    let point_84 = new Point({
+        y: 2886472,
+        x: 430735,
+        spatialReference: SpatialReference.WGS84
+    });
+    const outSpatialReference = new SpatialReference({
+        wkid: 2437
+    });
+    const point_2437 = projection.project(point_84,
+        outSpatialReference);
+    console.log("test", point_2437);
+});
+
 
 export class WebMapComponent extends React.Component {
   componentDidMount() {
